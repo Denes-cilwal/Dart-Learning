9d09cc682c983267f5de3944e8a48f2b144e832b
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-09-28 11:09:46 +0545

    intial commit - datatypes

---
+++ comment.dart
@@ -0,0 +1,9 @@
+void main() {
+  // single line comment -
+  // my app
+  print("hello world"); // print to console - compiler ignores this
+
+  /* 
+   // this is not encouraged 
+  */
+}
---
+++ datatypes.dart
@@ -0,0 +1,30 @@
+void main(List<String> arguments) {
+  // Numbers: int
+  int score = 23;
+  var count = 23; // It is inferred as integer automatically by compiler
+  // int hexValue = 0xEADEBAEE;
+
+  // Numbers: double
+  // double percentage = 93.4;
+  // var percent = 82.533;
+  double exponents = 1.42e5;
+
+  // Strings
+  String name = "Henry";
+  var company;
+
+  // Boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  print(score);
+  print(exponents);
+  print(count);
+  print(isAlive);
+  print(isValid);
+  print(company);
+  print(name);
+
+  // NOTE: All data types in Dart are Objects.
+  // Therefore, their initial value is by default 'null'
+}
